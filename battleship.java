204bf1f55b1e34eae7a5f813d89b1d244e58526c
ShainaR <S1371180@student.mcckc.edu>
2016-11-30 15:24:26 -0600

    Update battleship.java

--- battleship.java
+++ battleship.java
@@ -6,25 +6,14 @@ import java.util.Random;
 
 public class battleship {
 	
-	
+	static Gameboard gameboard1;
 	public static void main(String[] args) {
-		int size = 0;
-		int x=0;
-		char[][] gameBoard1 = null;
-		Random rand = new Random();
-		int temp = rand.nextInt(5) + 1;
-		ArrayList<String> location = new ArrayList<String>();
-		
-		
-		
-		
-		Scanner input = new Scanner(System.in);
-		
-		Gameboard gameboard1 = new Gameboard();
+		gameboard1 = new Gameboard();
 		
 		gameboard1.difficultyInfo();
-		gameboard1.outputGame();
+		gameboard1.positionShips();
 		
-		gameboard1.setShips(location);	
-  }
+		
+	}
+
 }
